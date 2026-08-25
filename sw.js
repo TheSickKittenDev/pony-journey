916062f77f7c88f11fe2f1efa25faa0c680a986c
The Sick Kitten <mrjackbuck@gmail.com>
2026-08-24 19:21:45 -0700

    Add missing PWA and style assets for full site load

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",e=>{e.waitUntil(caches.open("pony-v48").then(c=>c.addAll(["/","/index.html","/styles.css","/game.js","/data.js","/manifest.webmanifest","/icon-192.svg","/icon-512.svg"]))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
