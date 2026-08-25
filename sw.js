ba692cf3d9cc55bcb4253be1bd942e7097ee704f
The Sick Kitten <mrjackbuck@gmail.com>
2026-08-24 20:19:17 -0700

    Bump service worker cache to pony-journey-v47 and clear old caches on activate

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-const CACHE="pony-journey-v46";const ASSETS=["./","./index.html","./game.js","./data.js","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match("./index.html")))));
+const CACHE="pony-journey-v47";
+const ASSETS=["./","./index.html","./game.js","./data.js","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+self.addEventListener("fetch",e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    let cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res;
+  }).catch(()=>caches.match("./index.html")))
+));
